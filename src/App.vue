bda9695641f01b47af558f9b731572e44c495412
giftyinyang <caomiehinyang@gmail.com>
2020-01-25 21:25:40 +0100

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <Sidebar/>
+    <router-view/>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
+<script>
+import Sidebar from "@/components/Sidebar.vue";
+export default {
+  components: {Sidebar}
+}
+</script>
